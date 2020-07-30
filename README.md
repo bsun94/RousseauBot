9311cf011e2bc6b55ee8caa011d8b17f47f08c5e
bsun94 <52805678+bsun94@users.noreply.github.com>
2020-07-30 00:01:36 -0400

    Updates to script and readme files.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 My first Twitter Bot
 The two programs in this repository are behind @RousseauSays on Twitter.
 
-The corpusBuilder script scrapes a website containing the full text from Rousseau's "On the Social Contract", which is then massaged for formatting and to retain only those quotes short enough to fit Twitter's character limit.
+The corpusBuilder script scrapes a Online Library of Liberty webpage (https://oll.libertyfund.org/titles/rousseau-the-social-contract-and-discourses) containing the full text from Rousseau's "On the Social Contract", which it then splits into individual sentences and cleans before exporting into a txt (called "corpus") in your current directory. This script is notably dependent on the requests and bs4 modules.
 
-The RousseauBot script then reads the "corpus" built by the above and randomly selected a quote/sentence to post to Twitter. It keeps track of what it has posted the previous 10 times to avoid repetition (with certainty) posts over a short/medium period of time.
+This corpus.txt file serves as an input to the RousseauBot script.
+
+The RousseauBot script is dependent on a JSON file containing the credentials to RousseauBot's Twitter account (unfortunately not shared), which it reads and uses in accessing the account (API/access keys/tokens). It then reads the corpus file built by the above and randomly selects a quote/sentence to post to Twitter. It keeps track of what it has posted the previous 10 times to avoid repetition (with certainty) posts over a short/medium period of time by creating and exporting a pickle of an array into your current directory (which it then reads upon subsequent executions). This script is notably dependent on the tweepy, json and numpy modules.
--- RousseauBot.py
+++ RousseauBot.py
@@ -1,64 +1,76 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Jul 25 11:57:38 2020
 
-@author: Owner
+@author: BrianSun
+
+Tweets quotes of Rousseau from his book, "On the Social Contract"
 """
 
-# import all the modules we'll need, notably tweepy to interact with Twitter API
+# Import libraries, especially tweepy to interact with Twitter API
 import tweepy as tw
 import numpy as np
 import pickle as pk
 import os
+import sys
+import json
 
-# set current folder to be the working directory
+# Set current folder to be the working directory
 os.chdir(os.getcwd())
 
-# initialize a list to read in the RousseauBot Twitter account's credentials; saved into a txt to avoid displaying them in the code
-credList = []
-with open('APICredentials.txt', 'r') as file:
-    while True:
-        line = file.readline()
-        # get rid of newline characters in each line
-        credList += [line.strip('\n')]
-        # break out once finished reading file
-        if not line:
-            break
+# RousseauBot's Twitter credentials are stored in a json file, read/stored into variables below
+with open('APICredentials.json', 'r') as file:
+    credDict = json.load(file)
 
-# search for indices where the label for each credential is stored
-apiKeyInd, apiSecretInd, accTokenInd, accTokSecretInd = \
-    credList.index('API Key'), credList.index('API Secret Key'), credList.index('Access Token'), credList.index('Access Token Secret')
-# then get each credential as the succeeding item to each label above
-apiKey, apiSecret, accessToken, accessTokSecret = \
-    credList[apiKeyInd + 1], credList[apiSecretInd + 1], credList[accTokenInd + 1], credList[accTokSecretInd + 1]
+apiKey          = credDict['API Key']
+apiSecret       = credDict['API Secret Key']
+accessToken     = credDict['Access Token']
+accessTokSecret = credDict['Access Token Secret']
 
-# read in the corpus file, containing Rousseau's "On the Social Contract" broken out into lines
-with open('corpus.txt', 'r') as file:
-    corpus = file.readlines()
-# read in a pickled list, keeping track of previous lines from the corpus that was quoted in the past 10 days
-# this helps prevent the possibility of repeating the same line within 10 days
-with open('prevIndices.txt', 'rb') as indFile:
-    prevInd = pk.load(indFile)
+# Read in the corpus file, containing Rousseau's "On the Social Contract" broken out into lines
+try:
+    with open('corpus.txt', 'r') as file:
+        corpus = file.readlines()
+except:
+    sys.exit("Corpus file not found or unavailable.")
 
-# generate a random number such as we won't index a line from the corpus that was already indexed in the past 10 days
-ind = np.random.randint(0, len(corpus)-1)
-while ind in prevInd:
-    ind = np.random.randint(0, len(corpus)-1)
-# store current index into the previous indices list to be pickled, for keeping track going forward
-prevInd.append(ind)
-# just want to keep track over 10 days
-if len(prevInd)>10:
-    prevInd.pop(0)
-# pickle the previous indices list
-with open('prevIndices.txt', 'wb') as indFile:
-    pk.dump(prevInd, indFile)
+# Read in a pickled list, keeping track of previous lines from the corpus that was quoted in the past 10 days
+# This helps prevent the possibility of repeating the same line within 10 days
+try:
+    with open('prevIndices.txt', 'rb') as indexFile:
+        prevIndex = pk.load(indexFile)
+except:
+    prevIndex = []
 
-quoteToTweet = corpus[ind].strip('\n')
+# Generate a random number such as we won't index a line from the corpus that was already indexed in the past 10 days
+index = np.random.randint(0, len(corpus)-1)
+while index in prevIndex:
+    index = np.random.randint(0, len(corpus)-1)
+    
+# Store current index into the previous indices list to be pickled, for keeping track going forward
+prevIndex.append(index)
 
-# authenticate credentials with Twitter to access RousseauBot's account
-auth = tw.OAuthHandler(apiKey, apiSecret)
-auth.set_access_token(accessToken, accessTokSecret)
+# Just want to keep track over 10 days
+if len(prevIndex) > 10:
+    prevIndex.pop(0)
+    
+# Pickle the previous indices list
+with open('prevIndices.txt', 'wb') as indexFile:
+    pk.dump(prevIndex, indexFile)
+
+quoteToTweet = corpus[index].strip('\n')
+
+# Authenticate credentials with Twitter to access RousseauBot's account
+try:
+    auth = tw.OAuthHandler(apiKey, apiSecret)
+except:
+    sys.exit("API request rejected by Twitter.")
+
+try:
+    auth.set_access_token(accessToken, accessTokSecret)
+except:
+    sys.exit("Authentication failed.")
+    
 api = tw.API(auth)
 
-# tweet on RousseauBot's account
+# Tweet on RousseauBot's account
 api.update_status(quoteToTweet)
