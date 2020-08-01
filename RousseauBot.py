90c4d3f18bffcdab1aaeb1005fa4b9cba83d8092
bsun94 <52805678+bsun94@users.noreply.github.com>
2020-08-01 13:18:07 -0400

    Files converted to class structure commited

--- RousseauBot.py
+++ RousseauBot.py
@@ -14,63 +14,99 @@ import os
 import sys
 import json
 
-# Set current folder to be the working directory
-os.chdir(os.getcwd())
-
-# RousseauBot's Twitter credentials are stored in a json file, read/stored into variables below
-with open('APICredentials.json', 'r') as file:
-    credDict = json.load(file)
-
-apiKey          = credDict['API Key']
-apiSecret       = credDict['API Secret Key']
-accessToken     = credDict['Access Token']
-accessTokSecret = credDict['Access Token Secret']
-
-# Read in the corpus file, containing Rousseau's "On the Social Contract" broken out into lines
-try:
-    with open('corpus.txt', 'r') as file:
-        corpus = file.readlines()
-except:
-    sys.exit("Corpus file not found or unavailable.")
-
-# Read in a pickled list, keeping track of previous lines from the corpus that was quoted in the past 10 days
-# This helps prevent the possibility of repeating the same line within 10 days
-try:
-    with open('prevIndices.txt', 'rb') as indexFile:
-        prevIndex = pk.load(indexFile)
-except:
-    prevIndex = []
-
-# Generate a random number such as we won't index a line from the corpus that was already indexed in the past 10 days
-index = np.random.randint(0, len(corpus)-1)
-while index in prevIndex:
-    index = np.random.randint(0, len(corpus)-1)
+class RousseauTweets:
     
-# Store current index into the previous indices list to be pickled, for keeping track going forward
-prevIndex.append(index)
-
-# Just want to keep track over 10 days
-if len(prevIndex) > 10:
-    prevIndex.pop(0)
+    def __init__(self, credentialFile, corpusFile, previousTweets):
+        self.credentialFile = credentialFile
+        self.corpusFile = corpusFile
+        self.previousTweets = previousTweets
+        self.apiKey = ''
+        self.apiSecret = ''
+        self.accessToken = ''
+        self.accessTokSecret = ''
+        self.corpus = []
+        self.previousIndices = []
     
-# Pickle the previous indices list
-with open('prevIndices.txt', 'wb') as indexFile:
-    pk.dump(prevIndex, indexFile)
-
-quoteToTweet = corpus[index].strip('\n')
-
-# Authenticate credentials with Twitter to access RousseauBot's account
-try:
-    auth = tw.OAuthHandler(apiKey, apiSecret)
-except:
-    sys.exit("API request rejected by Twitter.")
-
-try:
-    auth.set_access_token(accessToken, accessTokSecret)
-except:
-    sys.exit("Authentication failed.")
+    # Set current folder to be the working directory
+    def updateDir(self):
+        os.chdir(os.getcwd())
     
-api = tw.API(auth)
+    # RousseauBot's Twitter credentials are stored in a json file, read/stored into variables below
+    def readCredentials(self):
+        with open(self.credentialFile, 'r') as file:
+            credDict = json.load(file)
 
-# Tweet on RousseauBot's account
-api.update_status(quoteToTweet)
+        self.apiKey          = credDict['API Key']
+        self.apiSecret       = credDict['API Secret Key']
+        self.accessToken     = credDict['Access Token']
+        self.accessTokSecret = credDict['Access Token Secret']
+    
+    # Read in the corpus file, containing Rousseau's "On the Social Contract" broken out into lines
+    def readQuotes(self):
+        try:
+            with open(self.corpusFile, 'r') as file:
+                self.corpus = file.readlines()
+        except:
+            sys.exit("Corpus file not found or unavailable.")
+    
+    # Read in a pickled list, keeping track of previous lines from the corpus that was quoted in the past 10 days
+    # This helps prevent the possibility of repeating the same line within 10 days
+    def readPreviousIndices(self):
+        try:
+            with open(self.previousTweets, 'rb') as indexFile:
+                self.previousIndices = pk.load(indexFile)
+        except:
+            self.previousIndices = []
+    
+    
+    def pickTrackTweet(self):
+        
+        # Generate a random number such as we won't index a line from the corpus that was already indexed in the past 10 days
+        index = np.random.randint(0, len(self.corpus)-1)
+        while index in self.previousIndices:
+            index = np.random.randint(0, len(self.corpus)-1)
+        
+        # Store current index into the previous indices list to be pickled, for keeping track going forward
+        self.previousIndices.append(index)
+        
+        # Just want to keep track over 10 days
+        if len(self.previousIndices) > 10:
+            self.previousIndices.pop(0)
+        
+        quoteToTweet = self.corpus[index].strip('\n')
+        
+        # Pickle the previous indices list
+        with open(self.previousTweets, 'wb') as indexFile:
+            pk.dump(self.previousIndices, indexFile)
+            
+        return quoteToTweet
+    
+    # Authenticate credentials with Twitter to access RousseauBot's account
+    def twitterAuth(self, quote):
+        try:
+            auth = tw.OAuthHandler(self.apiKey, self.apiSecret)
+        except:
+            sys.exit("API request rejected by Twitter.")
+                
+        try:
+            auth.set_access_token(self.accessToken, self.accessTokSecret)
+        except:
+            sys.exit("Authentication failed.")
+        
+        api = tw.API(auth)
+        api.update_status(quote)
+    
+    def runAll(self):
+        self.updateDir()
+        self.readCredentials()
+        self.readQuotes()
+        self.readPreviousIndices()
+        self.twitterAuth(self.pickTrackTweet())
+        
+# Define user-updatable constants for files containing credentials, quotes, previous tweets; then run
+CREDENTIALS_FILE = 'APICredentials.json'
+CORPUS_FILE = 'corpus.txt'
+PREVIOUSLY_TWEETED = 'prevIndices.txt'
+
+rousseau1 = RousseauTweets(CREDENTIALS_FILE, CORPUS_FILE, PREVIOUSLY_TWEETED)
+rousseau1.runAll()
--- corpusBuilder.py
+++ corpusBuilder.py
@@ -11,53 +11,77 @@ import requests
 import os
 import re
 
-# Set working directory to current folder; need to export sentences from the book into a txt later
-os.chdir(os.getcwd())
+class RousseauScraper:
+    
+    def __init__(self, startParagraph, endParagraph, twitterLimit, url):
+        self.startParagraph = startParagraph
+        self.endParagraph = endParagraph
+        self.twitterLimit = twitterLimit
+        self.url = url
+        self.quotes = []
+    
+    # Set working directory to current folder; need to export sentences from the book into a txt later
+    def dirUpdater(self):
+        os.chdir(os.getcwd())
+    
+    # Send a request to the website; get html and use BS to parse
+    def getHTML(self):
+        html = requests.get(self.url).text
+        soup = BeautifulSoup(html, "lxml")
+        return soup
+    
+    # Populates the object's quotes array
+    def HTMLparser(self):
+        soup = self.getHTML()
+        
+        # Sort through all the text in the html:
+        for text in soup.find_all('p'):
+            try:
+                paragraphNo = int(text.parent.p['id'][14:])
+                
+                # Only grab paragraphs in "On the Social Contract"
+                if paragraphNo < self.startParagraph or paragraphNo > self.endParagraph:
+                    continue
+                
+                elif text.string:
+                    
+                    # Ignore those "paragraphs" in the html that simply outline different chapters/books
+                    if re.search('^(CHAPTER|BOOK)(.*):', text.string):
+                        continue
+                    
+                    else:
+                        
+                        # Want to read in the document by sentence (for RousseauBot to use individually later on)
+                        tempList = re.split('(?<!etc)\.\s(?!.*\")|\!', text.string)
+                        for sentence in tempList:
+                            
+                            # When a "paragraph" is just a single sentence, re's .split() returns the sentence and a ''
+                            # Also, remove overly long quotes - Twitter has char limit
+                            if sentence != '' and len(sentence.strip()) < TWITTER_LIMIT:
+                                self.quotes.append(sentence.strip())
+                                
+            except KeyError:
+                
+                # BS throws KeyError when <p>'s id field is blank; ignore - all paragraphs I need has an id
+                continue
+        
+    # Write into the corpus file
+    def corpusWriter(self):
+        with open('corpus.txt', 'w') as file:
+            for quote in self.quotes:
+                file.write(quote + '\n')
+    
+    def runAll(self):
+        self.dirUpdater()
+        self.HTMLparser()
+        self.corpusWriter()
 
-# Send a request to the website; get html and use BS to parse
-url = 'https://oll.libertyfund.org/titles/rousseau-the-social-contract-and-discourses'
-html = requests.get(url).text
-soup = BeautifulSoup(html, "lxml")
 
-quotes = []
-
-# These paragraph numbers outline the range of "On the Social Contract" <p> tags
+# Define user-editable constants to be passed into class object as method arguments, then run
 START_PARAGRAPH = 145
 END_PARAGRAPH = 588
 TWITTER_LIMIT = 280
+URL = 'https://oll.libertyfund.org/titles/rousseau-the-social-contract-and-discourses'
 
-# Sort through all the text in the html:
-for text in soup.find_all('p'):
-    try:
-        paragraphNo = int(text.parent.p['id'][14:])
-        
-        # Only grab paragraphs in "On the Social Contract"
-        if paragraphNo < START_PARAGRAPH or paragraphNo > END_PARAGRAPH:
-            continue
-        
-        elif text.string:
-            
-            # Ignore those "paragraphs" in the html that simply outline different chapters/books
-            if re.search('^(CHAPTER|BOOK)(.*):', text.string):
-                continue
-            
-            else:
-                
-                # Want to read in the document by sentence (for RousseauBot to use individually later on)
-                tempList = re.split('(?<!etc)\.\s(?!.*\"$)|\!', text.string)
-                for sentence in tempList:
-                    
-                    # When a "paragraph" is just a single sentence, re's .split() returns the sentence and a ''
-                    # Also, remove overly long quotes - Twitter has char limit
-                    if sentence != '' and len(sentence.strip()) < TWITTER_LIMIT:
-                        quotes.append(sentence.strip())
-    
-    except KeyError:
-        
-        # BS throws KeyError when <p>'s id field is blank; ignore - all paragraphs I need has an id
-        continue
-
-# Write into the corpus file
-with open('corpus.txt', 'w') as file:
-    for quote in quotes:
-        file.write(quote + '\n')
+corpus1 = RousseauScraper(START_PARAGRAPH, END_PARAGRAPH, TWITTER_LIMIT, URL)
+corpus1.runAll()
